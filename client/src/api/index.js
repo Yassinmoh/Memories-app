9699679363c4bb2c8947125a9f0833c5fc2b32f5
yassin mohamed <yassin_mohamed310@outlook.com>
2022-03-22 15:40:52 +0200

    deploy APIs

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const url ='http://localhost:5000/posts'
+const url ='https://memor-app.herokuapp.com/posts'
 export const fetchPosts=()=>{
     return axios.get(url)
 }
